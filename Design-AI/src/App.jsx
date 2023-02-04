b1e6141be135f08eb233d03aa7e13699ef6f644c
sobunkohla <sobunkohla@gmail.com>
2023-02-04 17:45:49 +0200

    Adding functionality

--- Design-AI/src/App.jsx
+++ Design-AI/src/App.jsx
@@ -1,28 +1,47 @@
-import { useState } from 'react'
-import { Configuration, OpenAIApi } from 'openai'
-import './App.css'
+import { useState } from "react";
+import { Configuration, OpenAIApi } from "openai";
+import "./App.css";
 
 function App() {
+  const [prompt, setPrompt] = useState("");
+  const [result, setResult] = useState("");
   const configuration = new Configuration({
-    apiKey: import.meta.env.VITE_Open_AI_Key,
-  }); 
-
+    apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+  });
 
   const openai = new OpenAIApi(configuration);
 
-  const generateImage = async() => {
-    await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: "Say this is a test",
-      temperature: 0,
-      max_tokens: 7,
+  const generateImage = async () => {
+    const res = await openai.createImage({
+      prompt: prompt,
+      n: 1,
+      size: "1024x1024",
     });
-  }
+    setResult(res.data.data[0].url);
+  };
   return (
-    <div>
-      
+    <div className="app-container">
+      <h1 className="app-header">IMAGEN</h1>
+      <p className="app-description">
+        {" "}
+        from Thought, to text then into reality. Bring your Ideas to life with
+        IMAGEN the image generator you need to menifest your thoughts to
+        reality. with the power of AI type in the Image you want generated and
+        let the magic happen
+      </p>
+      <div className="app-main">
+        <input className="app-input" 
+        onChange={(e) => setPrompt(e.target.value)}
+        placeholder="Type desired image e.g 'a white siamese cat' "
+        />
+        <button className="app-button" onClick={generateImage}>
+          {" "}
+          →{" "}
+        </button>
+      </div>
+      {result.length > 0 ? <img src={result} alt="Result" className="result-img" />: <></>}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
